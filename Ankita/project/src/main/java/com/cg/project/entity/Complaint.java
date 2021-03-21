00cad57f1b506ffbd81ca74b3a1aadfd0cf91a26
AnkitaSagar <ankita.sinha8072@gmail.com>
2021-03-21 23:08:03 +0530

    Day0

---
+++ Ankita/project/src/main/java/com/cg/project/entity/Complaint.java
@@ -0,0 +1,112 @@
+package com.cg.project.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="complaint")
+public class Complaint {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int complaintId;
+	
+	@Column(name = "c_no")
+	private int consignmentNo;
+	
+	private String shortDescription;
+	
+	private String detailDescription;
+
+	public Complaint() {
+		
+	}
+
+	public Complaint(int complaintId, int consignmentNo, String shortDescription, String detailDescription) {
+		super();
+		this.complaintId = complaintId;
+		this.consignmentNo = consignmentNo;
+		this.shortDescription = shortDescription;
+		this.detailDescription = detailDescription;
+	}
+
+	
+	public int getComplaintId() {
+		return complaintId;
+	}
+
+
+	public int getConsignmentNo() {
+		return consignmentNo;
+	}
+
+	public void setConsignmentNo(int consignmentNo) {
+		this.consignmentNo = consignmentNo;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getDetailDescription() {
+		return detailDescription;
+	}
+
+	public void setDetailDescription(String detailDescription) {
+		this.detailDescription = detailDescription;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + complaintId;
+		result = prime * result + consignmentNo;
+		result = prime * result + ((detailDescription == null) ? 0 : detailDescription.hashCode());
+		result = prime * result + ((shortDescription == null) ? 0 : shortDescription.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Complaint other = (Complaint) obj;
+		if (complaintId != other.complaintId)
+			return false;
+		if (consignmentNo != other.consignmentNo)
+			return false;
+		if (detailDescription == null) {
+			if (other.detailDescription != null)
+				return false;
+		} else if (!detailDescription.equals(other.detailDescription))
+			return false;
+		if (shortDescription == null) {
+			if (other.shortDescription != null)
+				return false;
+		} else if (!shortDescription.equals(other.shortDescription))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Complaint [complaintId=" + complaintId + ", consignmentNo=" + consignmentNo + ", shortDescription="
+				+ shortDescription + ", detailDescription=" + detailDescription + "]";
+	}
+	
+	
+
+}
---
+++ Ankita/project/src/main/java/com/cg/project/entity/CourierStatus.java
@@ -0,0 +1,7 @@
+package com.cg.project.entity;
+
+public enum CourierStatus {
+	
+	INITIATED, INTRANSIT, DELIVERED, REJECTED
+
+}
