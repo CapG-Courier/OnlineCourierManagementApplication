b532013b4e03b6f57f14f5bd5304247e03b48c15
NagisettySreeja <siri199901@gmail.com>
2021-03-23 12:43:13 +0530

    updated

---
+++ Sreeja/OnlineCourierManagementApplication/src/main/java/com/cg/mts/exception/StaffMemberNotFoundException.java
@@ -0,0 +1,12 @@
+package com.cg.mts.exception;
+public class StaffMemberNotFoundException extends Exception {
+private static final long serialVersionUID=1L;
+	
+	public StaffMemberNotFoundException(String errorMsg) {
+		super(errorMsg);
+	}
+	
+
+}
+
+
