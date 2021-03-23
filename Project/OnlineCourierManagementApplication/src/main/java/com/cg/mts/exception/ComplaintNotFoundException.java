2285e039add4ecf3d43996f96bb6f74f6f655294
karanthapa89 <vonrules89@gmail.com>
2021-03-23 14:26:45 +0530

    exception

---
+++ Project/OnlineCourierManagementApplication/src/main/java/com/cg/mts/exception/ComplaintNotFoundException.java
@@ -0,0 +1,11 @@
+
+package com.cg.mts.exception;
+public class ComplaintNotFoundException extends Exception {
+private static final long serialVersionUID=1L;
+	
+	public ComplaintNotFoundException(String errorMsg) {
+		super(errorMsg);
+	}
+	
+
+}
